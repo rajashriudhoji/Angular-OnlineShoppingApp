be3b19e4981b51cf3ee1518a8c4a3cc1c2db7096
rajashriudhoji <rajashri.udhoji@synerzip.com>
2019-09-30 12:50:08 +0530

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,6 +21,12 @@ export class CartService {
     return this.items;
   }
 
-  constructor() {}
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
 }
